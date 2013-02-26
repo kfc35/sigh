6e3289e0b83580af6d3ce6230bf78862de678c0a
Sweet Song <shihui.sweet.song@gmail.com>
2013-02-26 12:00:08 -0500

    finished writing the terminator

--- src/sessions/CS5300PROJ1SESSION.java
+++ src/sessions/CS5300PROJ1SESSION.java
@@ -10,19 +10,27 @@ public class CS5300PROJ1SESSION extends Cookie {
 	//TODO 512 byte restriction on session state value
 	private String sessionId;
 	private String message;
+	private long end;
 	
-	public CS5300PROJ1SESSION(String s, String msg) {
+	public CS5300PROJ1SESSION(String s) {
 		super(s, s);
 		String[] args = s.split(";");
 		sessionId = args[0];
 		setVersion(Integer.parseInt(args[1]));
+		message = args[2];
+		end = Long.parseLong(args[3]);
+	}
+	
+	public long getEnd() {
+		return end;
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(sessionId).append(";");
-		sb.append(getVersion());
+		sb.append(getVersion()).append(";");
+		sb.append(message).append(";");
+		sb.append(end);
 		return sb.toString();
 	}
-
 }
---
+++ src/sessions/Terminator.java
@@ -0,0 +1,36 @@
+package sessions;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.concurrent.*;
+/**
+ * 
+ * @author sweet
+ * This class is responsible to removing over-time sessions from the session table
+ *
+ */
+public class Terminator implements Runnable {
+	private ConcurrentHashMap<String, CS5300PROJ1SESSION> sessionDataTable;
+
+	public Terminator(ConcurrentHashMap<String, CS5300PROJ1SESSION> sessionDataTable) {
+		this.sessionDataTable = sessionDataTable;
+	}
+
+	@Override
+	public void run() {
+		try{
+			this.wait(1000);
+			
+			HashMap<String, CS5300PROJ1SESSION> dup = new HashMap<String, CS5300PROJ1SESSION> (sessionDataTable);
+			for (Entry<String, CS5300PROJ1SESSION> e: dup.entrySet()) {
+				CS5300PROJ1SESSION cookie = e.getValue();
+				if (cookie.getEnd() < System.currentTimeMillis()) {
+					sessionDataTable.remove(e.getKey());
+				}
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+
+}
