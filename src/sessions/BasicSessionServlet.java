d185b1ba5465e231acec4ed7f38f731325b7d504
Kevin F. Chen <chen.kevin.f@gmail.com>
2013-02-26 19:42:26 -0500

    moved two lines

--- src/sessions/BasicSessionServlet.java
+++ src/sessions/BasicSessionServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BasicSessionServlet
  */
-@WebServlet("/BasicSessionServlet")
+@WebServlet("/")
 public class BasicSessionServlet extends HttpServlet {
 	public static boolean DEBUG = true;
 	private static final long serialVersionUID = 1L;
@@ -59,20 +59,24 @@ public class BasicSessionServlet extends HttpServlet {
 						  the cookie! Make a new cookie now!*/
 						break;
 					}
+					session.incrementVersion();
+					session.setEnd((new Date()).getTime() + EXPIRY_TIME_FROM_CURRENT);
 					
 					message = session.getMessage();
 					end = session.getEnd();
 					foundCookie = true;
 					if (DEBUG) {
-						System.out.println("Fetched Existing Session: " + session.toString());
+						System.out.println("Fetched Existing Session & Updated: " + session.toString());
 					}
+					//TODO updating the old cookie sent back to the client with a
+					// new expiration date? idk how this is possible.
 				}
 			}
 		}
 		if (!foundCookie) { //we have to create the session
 			UUID uuid = UUID.randomUUID();	//128 bits
-			message = DEFAULT_MESSAGE;			//current time + 2 minutes
-			end = (new Date()).getTime() + EXPIRY_TIME_FROM_CURRENT; //64 bits
+			message = DEFAULT_MESSAGE;
+			end = (new Date()).getTime() + EXPIRY_TIME_FROM_CURRENT; //64 bits, current time + 2 minutes
 			//TODO location metadata will be appended later.
 			
 			session = new CS5300PROJ1SESSION(uuid.toString(), message, end);
@@ -81,10 +85,6 @@ public class BasicSessionServlet extends HttpServlet {
 			}
 			sessionDataTable.put(uuid.toString(), session);
 			response.addCookie(new Cookie(CS5300PROJ1SESSION.COOKIE_NAME, uuid.toString()));
-		} else { // TODO: Kevin this is in the wrong closure, but I need to go
-			session.incrementVersion();
-			session.setEnd((new Date()).getTime() + EXPIRY_TIME_FROM_CURRENT);
-			//TODO UUID??? for add cookie
 		}
 		
 		response.setContentType("text/html");
